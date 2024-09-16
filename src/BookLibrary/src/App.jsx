ddc2cd1ed2dbaaaa3374b2d1a778bfe6b348ca51
Trdung0807 <trungdungg0807@gmail.com>
2024-09-16 16:38:45 +0700

    Header + Footer

--- src/BookLibrary/src/App.jsx
+++ src/BookLibrary/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="wrapper">
+      <div id="header">
+        <Navbar/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="main">
+        <h1>This is a Heading 1</h1>
+        <h1>This is a Heading 2</h1>
+        <h1>This is a Heading 3</h1>
+        <h1>This is a Heading 4</h1>
+        <h1>This is a Heading 5</h1>
+        <h1>This is a Heading 6</h1>
+        <h1>This is a Heading 7</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div id="footer">
+        <Footer/>
+      </div>
+    </div>
   )
 }
 
