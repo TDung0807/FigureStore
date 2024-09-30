cae7ea97dbf62339c213d5f6a0d0d6dbb7d4f4d5
Trdung0807 <trungdungg0807@gmail.com>
2024-09-30 09:57:46 +0700

    setup db for new

--- src/FigureStore_Api/db/migrate/20240926033339_create_figures.rb
+++ src/FigureStore_Api/db/migrate/20240926033339_create_figures.rb
@@ -1,7 +1,15 @@
 class CreateFigures < ActiveRecord::Migration[7.2]
   def change
     create_table :figures do |t|
-      t.timestamps
+      t.string :name
+      t.string :brand
+      t.string :material
+      t.decimal :price, precision: 8, scale: 2
+      t.integer :height
+      t.date :release_date
+      t.string :title
+      t.string :url
+      t.string :image_url
     end
   end
 end
