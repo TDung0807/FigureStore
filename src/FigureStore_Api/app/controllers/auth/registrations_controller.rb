22c958853401c9bea1cb1a2a960030536f32700c
Trdung0807 <trungdungg0807@gmail.com>
2024-10-21 11:50:42 +0700

    modify format

--- src/FigureStore_Api/app/controllers/auth/registrations_controller.rb
+++ src/FigureStore_Api/app/controllers/auth/registrations_controller.rb
@@ -1,25 +1,24 @@
 # app/controllers/auth/registrations_controller.rb
 module Auth
-    class RegistrationsController < ApplicationController
-      def create
-        user = User.create!(
-          email: params['user']['email'],
-          password: params['user']['password'],
-          password_confirmation: params['user']['password_confirmation']
-        )
-  
-        if user.persisted?
-          session[:user_id] = user.id
-          render json: {
-            status: :created,
-            user: user
-          }
-        else
-          render json: { status: 500 }
-        end
-      rescue ActiveRecord::RecordInvalid => e
-        render json: { status: 400, errors: e.record.errors.full_messages }
+  class RegistrationsController < ApplicationController
+    def create
+      user = User.create!(
+        email: params['user']['email'],
+        password: params['user']['password'],
+        password_confirmation: params['user']['password_confirmation']
+      )
+
+      if user.persisted?
+        session[:user_id] = user.id
+        render json: {
+          status: :created,
+          user: user
+        }
+      else
+        render json: { status: 500 }
       end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { status: 400, errors: e.record.errors.full_messages }
     end
   end
-  
+end
